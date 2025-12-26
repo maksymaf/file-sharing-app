2e6d60c13789f4a8586438b440bc0265d955458d
maksymaf <forbidden344@gmail.com>
2025-12-26 13:41:39 +0000

    chore: initialised basic project structure

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+async function main() {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('Successfuly connected to the database');
+        app.listen(PORT, () => {
+            console.log(`Server is listening on http://localhost:${PORT}`);
+        });
+    }catch(err){
+        console.error(err.stack);
+        process.exit(1)
+    }
+}
+
+main();
